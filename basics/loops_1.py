05d5260dff3fbb774ce03c5289f291657eebb1cb
RangelGasharov <rangel.gasharov@student.htldornbirn.at>
2023-10-16 08:16:12 +0200

    Loops #2 - fixed according to the given task

--- basics/loops_1.py
+++ basics/loops_1.py
@@ -1,5 +1,8 @@
 for i in range(1, 101):
     print(i)
 
-for i in range(2, 101, 2):
+sum_even_numbers = 0
+for i in range(2, 1001, 2):
+    sum_even_numbers += i
     print(i)
+print(sum_even_numbers)
