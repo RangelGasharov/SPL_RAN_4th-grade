08e2929dd5ecbb022d757ae35c9e4079aea47992
RangelGasharov <rangel.gasharov@student.htldornbirn.at>
2023-10-17 22:33:41 +0200

    Refactoring - made a folder for the opencv examples

--- opencv/color_spaces.py
+++ opencv/color_spaces.py
@@ -3,7 +3,11 @@ import cv2
 img = cv2.imread('images/road.jpg')
 
 img_rgb = cv2.cvtColor(img, cv2.COLOR_BGR2RGB)
+img_gray = cv2.cvtColor(img, cv2.COLOR_BGR2GRAY)
+img_hsv = cv2.cvtColor(img, cv2.COLOR_BGR2HSV)
 
 cv2.imshow("img", img)
 cv2.imshow("img_rgb", img_rgb)
+cv2.imshow("img_gray", img_gray)
+cv2.imshow("img_hsv", img_hsv)
 cv2.waitKey(0)
