255282c60a5fd506dc8158ddba08e87d7b58ef00
RangelGasharov <rangel.gasharov@student.htldornbirn.at>
2023-11-13 21:44:35 +0100

    Added a program for the traffic lights circuit

---
+++ rasberry/traffic_lights.py
@@ -0,0 +1,42 @@
+import RPi.GPIO as GPIO
+import time
+import signal
+import sys
+
+PIN_RED = 17
+PIN_YELLOW = 18
+PIN_GREEN = 27
+
+GPIO.setmode(GPIO.BCM)
+GPIO.setup(PIN_RED, GPIO.OUT)
+GPIO.setup(PIN_YELLOW, GPIO.OUT)
+GPIO.setup(PIN_GREEN, GPIO.OUT)
+
+
+def allLightsOff(signal, frame):
+    GPIO.output(PIN_RED, False)
+    GPIO.output(PIN_YELLOW, False)
+    GPIO.output(PIN_GREEN, False)
+    GPIO.cleanup()
+    sys.exit(0)
+
+
+signal.signal(signal.SIGINT, allLightsOff)
+
+while True:
+    GPIO.output(PIN_RED, True)
+    time.sleep(3)
+
+    GPIO.output(PIN_YELLOW, True)
+    time.sleep(1)
+
+    GPIO.output(PIN_RED, False)
+    GPIO.output(PIN_YELLOW, False)
+    GPIO.output(PIN_GREEN, True)
+    time.sleep(5)
+
+    GPIO.output(PIN_GREEN, False)
+    GPIO.output(PIN_RED, True)
+    time.sleep(2)
+
+    GPIO.output(PIN_YELLOW, False)
